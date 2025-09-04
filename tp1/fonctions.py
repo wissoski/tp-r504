b2418ab3e0c8a739ea2a33680256ba7282f4997e
wissoski <wissal.ouaziz1@gmail.com>
2025-09-04 17:22:07 +0200

    6,jsp

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,16 +1,16 @@
-def puissance(a: int, b: int) -> int. | . float:
-   if not isinstance(a, int) or. not. isinstance(b, int) :
+def puissance(a: int, b: int) -> int | float:
+   if not isinstance(a, int) or. not. isinstance(b, int):
    raise TypeError("Seuls. les entiers-sont autorisés")
-   if a == 0- and.b.<. 0:
-   raise ValueError ("On.ne-peut pas-avec-0")
-   if b = 0:
-   return. 1
-   if. b.> 0:
-   resultat = 1
-   for in range(b):
-   resultat *= a
-   return resultat
-   if.b.< 0:
-   resultat =1 for.
-   in. range(-b) :
-   resultat.*= a return. 1/ resultat
+     if a == 0- and b < 0:
+     raise ValueError ("O n'est pas une puissance")
+     if b == 0:
+     return 1
+     if b > 0:
+     resultat = 1
+     for in range(b):
+     resultat *= a
+     return resultat
+     if b < 0:
+     resultat =1 for.
+     in range(-b) :
+     resultat *= a return 1/ resultat
