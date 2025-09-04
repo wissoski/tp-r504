d23bd13306e09435453ac5081380f96d9e24cd63
wissoski <wissal.ouaziz1@gmail.com>
2025-09-04 16:54:03 +0200

    6.

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,9 +1,13 @@
 def puissance(a, b):
-    if not type(a) is int:
+    if not isinstance(a, int) or not isinstance(b, int):
         raise TypeError("Only integers are allowed")
-    if not type(b) is int:
-        raise TypeError("Only integers are allowed")
-    return a ** b
     if a == 0 and b < 0:
-        raise Execption("Puissance nulle")
-    return a ** b
+        raise Exception("0 à une puissance négative est indéfini")
+    if b < 0:
+        raise Exception("Exposant négatif interdit")
+    if b == 0:
+        return 1
+    resultat = 1
+    for _ in range(b):
+        resultat *= a
+    return resultat
