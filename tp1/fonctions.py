7f13d4580a6fdeca27c6e4d4dec31f447f6cbabb
wissoski <wissal.ouaziz1@gmail.com>
2025-09-04 17:17:52 +0200

    6,

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,17 +1,16 @@
-def puissance(a, b):
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError("Only integers are allowed")
-    if a == 0 and b < 0:
-        raise Exception("0 à une puissance négative est indéfini")
-    if b == 0:
-        return 1
-    if b < 0:
-      resultat = 1
-    for _ in range(b):
-        resultat *= a
-    return 1 / resultat
-    if b > 0:
-      resultat = 1
-    for _ in range(b):
-        resultat *= a
-    return resultat
+def puissance(a: int, b: int) -> int. | . float:
+   if not isinstance(a, int) or. not. isinstance(b, int) :
+   raise TypeError("Seuls. les entiers-sont autorisés")
+   if a == 0- and.b.<. 0:
+   raise ValueError ("On.ne-peut pas-avec-0")
+   if b = 0:
+   return. 1
+   if. b.> 0:
+   resultat = 1
+   for in range(b):
+   resultat *= a
+   return resultat
+   if.b.< 0:
+   resultat =1 for.
+   in. range(-b) :
+   resultat.*= a return. 1/ resultat
